33728913ddf866fab35eabc57f02d0b5fd7c12aa
Rahul Jha <errahuljha51@gmail.com>
2020-04-24 02:38:19 +0530

    Initial Commit

---
+++ Aura Framework/force-app/main/default/aura/handlerExample/handlerExampleController.js
@@ -0,0 +1,16 @@
+({
+    submit : function(component, event, helper) {
+        alert('i have invoked submit button');
+
+
+    },
+
+    callMe : function(component,event,helper)
+    {
+
+
+        alert('i have invoked callme button');
+
+
+    }
+})
